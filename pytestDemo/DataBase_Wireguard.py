cf94470706b706068fe33e4c99c2e6b74ff7357c
jyoti.y <jyoti@customer-acquisition.co>
2023-05-08 23:16:49 +0530

    Push Changes

--- pytestDemo/DataBase_Wireguard.py
+++ pytestDemo/DataBase_Wireguard.py
@@ -1,23 +1,13 @@
 import mysql.connector
 from mysql.connector import Error
 
-try:
-    connection = mysql.connector.connect(host='private-leadgen-mysql-ny-1-read-only-90023-do-user-7297834-0.a.db.ondigitalocean.com',
+connection = mysql.connector.connect(host='private-leadgen-mysql-ny-1-read-only-2-do-user-7297834-0.b.db.ondigitalocean.com',
                                          database='leadgen',
                                          user='jyoti.y_ro',
-                                         password='E#twrT#$#%%^TEGE3rse%^')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select ;")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
+                                         password='E#twrT#$#%%^TEGE3rse%^',
+                                         port = 25060
+                                     )
 
-except Error as e:
-    print("Error while connecting to Database", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("Connection")
+connection.close()
+
+
---
+++ pytestDemo/Read_write_file.py
@@ -0,0 +1,4 @@
+a = open("C:\\Users\\jyoti.y\\Downloads\\autoaccident.text", 'w+')
+a.write("Hello Jamie")
+# print(a.read())
+print(a.readline())
